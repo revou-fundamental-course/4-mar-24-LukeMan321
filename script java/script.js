1c0b9ffa6ed3680c85ba7b2da2339e788e9a5946
Muhammad Luqman Firdaus <amanahluqman321@gmail.com>
2024-03-13 23:37:54 +0700

    init commit

--- script java/script.js
+++ script java/script.js
@@ -1,2 +1,11 @@
-// javascript
+// Kode Validasi form berat badan
+function calculate() {
+    let weightInput = document.getElementById("weight")
+    console.log(weightInput.value)
 
+    if (weightInput.isNaN) {
+    console.log('Number')
+    } else {
+        console.log('Not Number')
+    }   
+}
